053bfaa4733e31428670801f6ce92ca56d4f2c87
Leo Locatelli <leolocatelli30@gmail.com>
2025-03-23 18:36:00 +0100

    🐛 Fix Puppeteer Chrome path for Render

--- backend/analyzePageAccessibility.js
+++ backend/analyzePageAccessibility.js
@@ -5,8 +5,8 @@ async function analyzePageAccessibility(url) {
   let browser;
 
   try {
+    // Heroku uses a custom Chrome path
     const isHeroku = process.env.NODE_ENV === "production" && process.env.RENDER !== "true";
-    const isRender = process.env.RENDER === "true";
 
     browser = await puppeteer.launch({
       headless: true,
@@ -21,9 +21,9 @@ async function analyzePageAccessibility(url) {
         "--single-process",
         "--disable-extensions"
       ],
-      executablePath: isHeroku ? "/app/.apt/usr/bin/google-chrome-stable" : undefined,
+      executablePath: isHeroku ? "/app/.apt/usr/bin/google-chrome-stable" : undefined
     });
-// teste
+
     const page = await browser.newPage();
     await page.setBypassCSP(true);
 
